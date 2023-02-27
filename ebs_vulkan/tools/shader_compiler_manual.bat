8db99bcac198f5f97d3f8e1f50cb7f94bf0d59e2
EricB326 <ericbrkic326@gmail.com>
2023-02-27 20:20:28 +1100

    Added PreBuild event for compiling shaders using batch file. Also added a manual alternative if need be.

---
+++ ebs_vulkan/tools/shader_compiler_manual.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+set glslc=%VK_SDK_PATH%\Bin\glslc.exe
+set shader_path=..\shaders
+set shader_stages="*.vert","*.frag"
+
+cd %shader_path%
+for %%v in (%shader_stages%) do (
+	%glslc% ./%%v -o ./%%v.spv
+	ECHO [SHADER COMPILER] Compiling %%v to %%v.spv
+)
+
+pause
